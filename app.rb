400b1d531ddc4ab47d49712711ebe356fcdf487a
laww10 <114757219+laww10@users.noreply.github.com>
2024-04-10 01:35:22 +0000

    correct_output_failing_tests

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:input_form, {:layout => :home})
+end
+
+get("/process_roll") do
+
+  @num_dice = params.fetch("number").to_i
+  @sides = params.fetch("sides").to_i
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+    
+  end
+
+  erb(:flexible, {:layout => :body})
 end
